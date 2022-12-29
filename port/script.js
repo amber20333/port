83fee4726aa8d212aa1ba7a8fe600760050ff383
Amber Azerad <113435737+amber20333@users.noreply.github.com>
2022-12-29 19:21:49 +0200

    Add files via upload

---
+++ port/script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('contact');
+
+button.addEventListener('click',function(){
+    window.open("mailto:ambre.azerad3@gmail.com");
+
+})
+
+let button2 = document.getElementById('btn');
+
+button2.addEventListener('click',function(){
+    window.open("https://github.com/amber20333")
+
+});
